022ce8209135feccc9493331291c2e2d6b81953c
Yu ZhiHuan <yuzhihu1@teach.cs.toronto.edu>
2019-12-02 18:02:00 -0500

    2bsub

---
+++ A3/twosub.pl
@@ -0,0 +1,48 @@
+:- ale_flag(subtypecover,_,off).
+:- discontiguous sub/2,intro/2.
+
+bot sub [mood, tense, sem, cat, pos, verbal, nominal].
+
+    % parts of speech
+        pos sub [n, p, v, det, toinf].
+            n sub [].
+            p sub [].
+            v sub [].
+            det sub [].
+            toinf sub [].   % infinitival to
+    % phrasal categories
+    cat sub [vproj, np].
+        vproj sub [inf_clause, s, vp] intro [mood:mood].
+            inf_clause intro [mood:infinitive].
+            s intro [mood:indicative].
+            vp intro [mood:indicative].
+        np sub [].
+
+        verbal sub [v, vproj] intro [vsem:v_sem].
+        nominal sub [n, np] intro [nsem:n_sem].
+    
+    % mood and tense for verbs
+    tense sub [past, present].
+        past sub [].    
+        present sub [].
+    mood sub [indicative, infinitive].
+        indicative intro [tense:tense].
+        infinitive sub [].
+
+    % semantics for verbs and nouns
+    sem sub [v_sem, n_sem].
+
+        % semantics for nouns
+        n_sem sub [student, teacher].
+            student sub [].
+            teacher sub [].
+
+        % semantics for verbs 
+        v_sem sub [try, appear, promise, expect, sleep]
+              intro [].     % This should not be empty!  Fill in features for this and
+                            % the following subtypes:
+            try sub [].
+            appear sub [].
+            promise sub [].
+            expect sub [].
+            sleep sub [].
