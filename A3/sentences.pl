40d0248e28957afe7a18ccbf73c3fa91dec91f1a
zhihuan <zhihuan.yu@mail.utoronto.ca>
2019-12-03 21:12:54 -0500

    1

---
+++ A3/sentences.pl
@@ -0,0 +1,5 @@
+% Your name, login ID and student ID go here.
+test_sent([the,student,tried,to,sleep]).
+test_sent([the,student,appeared,to,sleep]).
+test_sent([the,student,promised,the,teacher,to,sleep]).
+test_sent([the,student,expected,the,teacher,to,sleep]).
